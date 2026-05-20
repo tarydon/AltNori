d37a725872d2c0770d4b3517fabad2327b5daeb6
arvind <arvind@fastmail.in>
2026-05-20 21:11:20 +0530

    Added query logic
    Provide a clean DeInit call
    
    Added flag to prevent double-Init

--- Lux/Init.cs
+++ Lux/Init.cs
@@ -6,7 +6,19 @@ using System.Reflection;
 namespace Nori;
 
 public class Lux2 {
-   public static void Init () {
-      VNode.RegisterAssembly (Assembly.GetExecutingAssembly ());
+   public static void Init () {      
+      if (!sInited) { 
+         sInited = true;
+         VNode.RegisterAssembly (Assembly.GetExecutingAssembly ());
+      }
    }
+
+   public static void DeInit () {
+      Lux.ShutDown (); 
+      sInited = false;
+   }
+
+   public static bool IsRunning => sInited;
+
+   static bool sInited;
 }
